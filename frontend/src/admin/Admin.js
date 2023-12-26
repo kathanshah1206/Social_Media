32e8928383e4a31b7c41a5b74831b40a0c245e1d
kathanshah1206 <97287116+kathanshah1206@users.noreply.github.com>
2023-12-26 16:29:25 +0530

    Update Admin.js

--- frontend/src/admin/Admin.js
+++ frontend/src/admin/Admin.js
@@ -1,48 +1,44 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Posts from "../post/Posts";
 import Users from "../user/Users";
 import { isAuthenticated } from "../auth";
 import { Redirect } from "react-router-dom";
 
-class Admin extends Component {
-    state = {
-        redirectToHome: false
-    };
+const Admin = () => {
+  const [redirectToHome, setRedirectToHome] = useState(false);
 
-    componentDidMount() {
-        if (isAuthenticated().user.role !== "admin") {
-            this.setState({ redirectToHome: true });
-        }
+  useEffect(() => {
+    if (isAuthenticated().user.role !== "admin") {
+      setRedirectToHome(true);
     }
+  }, []);
 
-    render() {
-        if (this.state.redirectToHome) {
-            return <Redirect to="/" />;
-        }
+  if (redirectToHome) {
+    return <Redirect to="/" />;
+  }
 
-        return (
-            <div>
-                <div className="jumbotron">
-                    <h2>Admin Dashboard</h2>
-                    <p className="lead">Welcome to React Frontend</p>
-                </div>
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <h2>Posts</h2>
-                            <hr />
-                            <Posts />
-                        </div>
-                        <div className="col-md-6">
-                            <h2>Users</h2>
-                            <hr />
-                            <Users />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+  return (
+    <div>
+      <div className="jumbotron">
+        <h2>Admin Dashboard</h2>
+        <p className="lead">Welcome to React Frontend</p>
+      </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6">
+            <h2>Posts</h2>
+            <hr />
+            <Posts />
+          </div>
+          <div className="col-md-6">
+            <h2>Users</h2>
+            <hr />
+            <Users />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Admin;
